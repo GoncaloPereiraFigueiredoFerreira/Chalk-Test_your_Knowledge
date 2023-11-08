b400bc23e50dc208e924fd4200691e455e55b8f4
Casal Novo <52653136+DCasalNovo@users.noreply.github.com>
2023-11-08 23:05:52 +0000

    Exercise type selector

--- Chalk-Frontend/src/components/interactiveElements/PopUp.tsx
+++ Chalk-Frontend/src/components/interactiveElements/PopUp.tsx
@@ -1,24 +1,29 @@
 import "./PopUp.css";
 
 type PopUpProps = {
+  show: boolean;
   children: JSX.Element;
   closePopUp: () => void;
 };
 
-export function PopUp({ children, closePopUp }: PopUpProps) {
+export function PopUp({ show, children, closePopUp }: PopUpProps) {
   return (
-    <>
-      <div className="fixed top-0 right-0 z-30 flex flex-col w-screen h-screen">
+    <div
+      className={`${
+        show ? "" : "translate-x-full"
+      } fixed top-0 right-0 z-30 duration-100`}
+    >
+      <div className="flex flex-col w-screen h-screen">
         <button className="popup-btn" onClick={() => closePopUp()} />
         <div className="flex w-full min-h-max">
           <button className="popup-btn" onClick={() => closePopUp()} />
-          <div className="min-w-max min-h-max p-3 rounded-lg bg-white dark:bg-gray-600">
+          <div className="min-w-max min-h-max p-6 rounded-xl bg-white shadow-2xl shadow-gray-800 dark:bg-gray-600">
             {children}
           </div>
           <button className="popup-btn" onClick={() => closePopUp()} />
         </div>
         <button className="popup-btn" onClick={() => closePopUp()} />
       </div>
-    </>
+    </div>
   );
 }
--- Chalk-Frontend/src/components/pages/FrontPage/FrontPage.tsx
+++ Chalk-Frontend/src/components/pages/FrontPage/FrontPage.tsx
@@ -2,16 +2,49 @@ import { Searchbar } from "../../objects/Searchbar/Searchbar";
 import { Exercise } from "../../objects/Exercise/Exercise";
 import "./FrontPage.css";
 import { useState } from "react";
+import { PopUp } from "../../interactiveElements/PopUp";
+import {
+  CheckboxIcon,
+  CheckedListIcon,
+  CodeIcon,
+  InputIcon,
+  TextIcon,
+} from "../../objects/SVGImages/SVGImages";
 
 export function FrontPage() {
   const [selectedExercise, setSelectedExercise] = useState(-1);
+  const [newExercisePopUp, setNewExercisePopUp] = useState(false);
+  const [newExercisetype, setNewExercisetype] = useState("multiple-choice");
+
+  function createNewExercise() {
+    // colocar aqui as chamadas para criação dos exercicios
+    switch (newExercisetype) {
+      case "multiple-choice":
+        break;
+      case "open-answer":
+        break;
+      case "true-or-false":
+        break;
+      case "fill-in-the-blank":
+        break;
+      case "code":
+        break;
+    }
+  }
+
   return (
     <div className="flex flex-col w-full dark:bg-gray-700 bg-white py-8 gap-8">
       <Searchbar></Searchbar>
       <div className="flex-col flex w-full h-screen dark:bg-gray-700 px-16">
-        <label className="flex text-4xl mb-4 px-4 pb-2.5 border-b border-b-gray-300 text-gray-500 dark:border-b-gray-500 dark:text-white">
-          Exercícios
-        </label>
+        <div className="flex w-full justify-between mb-4 px-4 pb-2.5 border-b border-b-gray-300 text-gray-600 dark:border-b-gray-500 dark:text-white">
+          <label className="flex text-4xl">Exercícios</label>
+          <button
+            className="transition-all duration-100 py-2 px-4 rounded-lg bg-gray-200 hover:bg-slate-300 dark:hover:bg-gray-800 dark:bg-gray-600"
+            onClick={() => setNewExercisePopUp(true)}
+          >
+            Criar exercício
+          </button>
+        </div>
         <Exercise
           name={"Quantas canetas"}
           visibility={"private"}
@@ -34,6 +67,82 @@ export function FrontPage() {
           setSelectedExercise={(value) => setSelectedExercise(value)}
         ></Exercise>
       </div>
+      <PopUp
+        show={newExercisePopUp}
+        closePopUp={() => setNewExercisePopUp(false)}
+        children={
+          <>
+            <label className="flex w-full justify-between mb-4 px-4 pb-2.5 border-b border-b-gray-300 text-gray-600 dark:border-b-gray-500 dark:text-white text-4xl">
+              Criar novo exercício
+            </label>
+            <div className="grid grid-cols-2">
+              <button
+                onClick={() => setNewExercisetype("multiple-choice")}
+                className={`${
+                  "multiple-choice" != newExercisetype
+                    ? "btn-normal-frontpage text-gray-500"
+                    : "btn-selected-frontpage text-white"
+                } btn-frontpage text-lg dark:text-white group`}
+              >
+                <CheckedListIcon style="inherit-icon" size="size-12" />
+                Escolha múltipla
+              </button>
+              <button
+                onClick={() => setNewExercisetype("open-answer")}
+                className={`${
+                  "open-answer" != newExercisetype
+                    ? "btn-normal-frontpage text-gray-500"
+                    : "btn-selected-frontpage text-white"
+                } btn-frontpage text-lg dark:text-white group`}
+              >
+                <TextIcon style="inherit-icon" size="size-12" />
+                Resposta aberta
+              </button>
+              <button
+                onClick={() => setNewExercisetype("true-or-false")}
+                className={`${
+                  "true-or-false" != newExercisetype
+                    ? "btn-normal-frontpage text-gray-500"
+                    : "btn-selected-frontpage text-white"
+                } btn-frontpage text-lg dark:text-white group`}
+              >
+                <CheckboxIcon style="inherit-icon" size="size-12" />
+                Verdadeiro ou falso
+              </button>
+              <button
+                onClick={() => setNewExercisetype("fill-in-the-blank")}
+                className={`${
+                  "fill-in-the-blank" != newExercisetype
+                    ? "btn-normal-frontpage text-gray-500"
+                    : "btn-selected-frontpage text-white"
+                } btn-frontpage text-lg dark:text-white group`}
+              >
+                <InputIcon style="inherit-icon" size="size-12" />
+                Preenchimento de espaços
+              </button>
+              <button
+                onClick={() => setNewExercisetype("code")}
+                className={`${
+                  "code" != newExercisetype
+                    ? "btn-normal-frontpage text-gray-500"
+                    : "btn-selected-frontpage text-white"
+                } btn-frontpage text-lg dark:text-white group`}
+              >
+                <CodeIcon style="inherit-icon" size="size-12" />
+                Código
+              </button>
+            </div>
+            <div className="flex justify-end">
+              <button
+                onClick={() => createNewExercise()}
+                className="btn-selected-frontpage btn-frontpage group"
+              >
+                <label className="text-lg text-white">Seguinte</label>
+              </button>
+            </div>
+          </>
+        }
+      />
     </div>
   );
 }
