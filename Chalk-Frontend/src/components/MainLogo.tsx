d1919d9ef88153ddc2f430e0a2a4f481b6b04f0b
GoncaloPereiraFigueiredoFerreira <a93218@alunos.uminho.pt>
2023-11-18 18:24:21 +0000

    Revert "Added Chalk logo to the sidebar"
    This reverts commit 7f706cb271700465510647914559796c9a6b00f3.

--- Chalk-Frontend/src/components/MainLogo.tsx
+++ Chalk-Frontend/src/components/MainLogo.tsx
@@ -1,16 +1,14 @@
-export function MainLogo(props: any) {
+export function MainLogo({}) {
   return (
     <>
-      <div className="flex items-center flex-grow select-none">
+      <div className="flex justify-center flex-grow">
         <svg
-          className="dark:invert"
           version="1.1"
-          id="oi"
+          id="Layer_1"
           xmlns="http://www.w3.org/2000/svg"
           x="0px"
           y="0px"
-          width={props.size === "big" ? "224" : "56"}
-          height={props.size === "big" ? "140" : "37"}
+          width="40%"
           viewBox="0 0 560 352"
           enableBackground="new 0 0 560 352"
         >
@@ -146,21 +144,9 @@ M129.038757,227.170013
 z"
           />
         </svg>
-        <div className="flex flex-col">
-          <p
-            className={`font-pacifico  dark:text-white  ${
-              props.size === "big" ? "text-9xl" : "text-xl"
-            }`}
-          >
-            Chalk
-          </p>
-          <p
-            className={`font-jetbrains dark:text-white   ${
-              props.size === "big"
-                ? "lg:text-5xl ml-5 text-3xl"
-                : "text-[0.5rem] ml-1"
-            }`}
-          >
+        <div className="flex flex-col mt-6">
+          <p className="font-pacifico text-9xl">Chalk</p>
+          <p className="font-jetbrains text-5xl ml-4 mt-3">
             Test Your Knowledge
           </p>
         </div>
--- Chalk-Frontend/src/components/objects/Sidebar/Sidebar.tsx
+++ Chalk-Frontend/src/components/objects/Sidebar/Sidebar.tsx
@@ -17,7 +17,6 @@ import {
 } from "../SVGImages/SVGImages.tsx";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { MainLogo } from "../../MainLogo.tsx";
 
 const groups: { [key: string]: string } = {
   "1": "Professores da escola AFS Gualtar",
@@ -93,8 +92,15 @@ export function Sidebar({
         >
           <SidebarIcon style={"group-gray-icon"} />
         </button>
-        <div className={` ${isOpen ? "" : "hidden"}`}>
-          <MainLogo></MainLogo>
+        <div className={`flex mr-4 ${isOpen ? "" : "hidden"}`}>
+          <img
+            src="https://flowbite.s3.amazonaws.com/logo.svg"
+            className="mr-3 h-8"
+            alt="FlowBite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Flowbite
+          </span>
         </div>
       </div>
       <ul className="sidebar-divisions border-t-0">
