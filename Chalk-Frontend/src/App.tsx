9d60f03d4a7e1e712cc7e004f83a6839f6f87f05
DivineWingedDemon <58912131+DivineWingedDemon@users.noreply.github.com>
2023-11-23 15:20:55 +0000

    added Subscription route

--- Chalk-Frontend/src/App.tsx
+++ Chalk-Frontend/src/App.tsx
@@ -17,7 +17,6 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/pricing" element={<Subscription />} />
-
         <Route path="/settings" element={<Settings />} />
         <Route path="/user" element={<Sidebar />}>
           <Route index element={<FrontPage />} />
