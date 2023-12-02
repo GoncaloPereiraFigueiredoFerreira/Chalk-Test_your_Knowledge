ad309c711913d18ab5a68a855663c0b2fa17cfd0
Alex-Sa-Ms <a93315@alunos.uminho.pt>
2023-12-02 15:38:25 +0000

    Updated institution service interface

--- Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/services/IInstitutionsService.java
+++ Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/services/IInstitutionsService.java
@@ -1,9 +1,14 @@
 package pt.uminho.di.chalktyk.services;
 
 import org.springframework.data.domain.Page;
+import pt.uminho.di.chalktyk.models.nonrelational.courses.Course;
 import pt.uminho.di.chalktyk.models.nonrelational.institutions.Institution;
+import pt.uminho.di.chalktyk.models.relational.Specialist;
+import pt.uminho.di.chalktyk.models.relational.Student;
 import pt.uminho.di.chalktyk.services.exceptions.BadInputException;
 import pt.uminho.di.chalktyk.services.exceptions.NotFoundException;
+
+import java.util.List;
 
 public interface IInstitutionsService {
 
@@ -43,4 +48,107 @@ public interface IInstitutionsService {
     * @param institution
     **/
     void createInstitution(Institution institution) throws BadInputException;
+
+    /**
+     * Add specialists to institution
+     * @param institutionId identifier of the institution
+     * @param specialistsIds list of specialists identifiers
+     */
+    void addSpecialistsToInstitution(String institutionId, List<String> specialistsIds);
+
+    /**
+     * Add students to institution.
+     * @param institutionId identifier of the institution
+     * @param studentsIds list of students identifiers
+     */
+    void addStudentsToInstitution(String institutionId, List<String> studentsIds);
+
+    /**
+     * Remove specialists from institution
+     * @param institutionId identifier of the institution
+     * @param specialistsIds list of specialists identifiers
+     */
+    void removeSpecialistsFromInstitution(String institutionId, List<String> specialistsIds);
+
+    /**
+     * Remove students from institution.
+     * @param institutionId identifier of the institution
+     * @param studentsIds list of students identifiers
+     */
+    void removeStudentsFromInstitution(String institutionId, List<String> studentsIds);
+
+    /**
+     * Checks if a student is associated with a specific institution.
+     * @param studentId identifier of the student
+     * @param institutionId identifier of the institution
+     * @return 'true' if the student is associated with the institution
+     */
+    boolean isStudentOfInstitution(String studentId, String institutionId);
+
+    /**
+     * Checks if a specialist is associated with a specific institution.
+     * @param specialistId identifier of the specialist
+     * @param institutionId identifier of the institution
+     * @return 'true' if the specialist is associated with the institution
+     */
+    boolean isSpecialistOfInstitution(String specialistId, String institutionId);
+
+    /**
+     * Checks if a manager is associated with a specific institution.
+     * @param managerId identifier of the manager
+     * @param institutionId identifier of the institution
+     * @return 'true' if the manager is associated with the institution
+     */
+    boolean isManagerOfInstitution(String managerId, String institutionId);
+
+    /**
+     * Get list of students that are associated with a specific institution.
+     * @param institutionId institution identifier
+     * @param page index of the page
+     * @param itemsPerPage number of items each page has
+     * @return list of students that are associated with a specific institution.
+     */
+    Page<Student> getInstitutionStudents(String institutionId, int page, int itemsPerPage);
+
+    /**
+     * Get list of specialists that are associated with a specific institution.
+     * @param institutionId institution identifier
+     * @param page index of the page
+     * @param itemsPerPage number of items each page has
+     * @return list of specialists that are associated with a specific institution.
+     */
+    Page<Specialist> getInstitutionSpecialists(String institutionId, int page, int itemsPerPage);
+
+    /**
+     * Get institution associated with a specific student
+     * @param studentId identifier of the student
+     * @return institution associated with a specific student
+     */
+    Institution getStudentInstitution(String studentId);
+
+    /**
+     * Get institution associated with a specific specialist
+     * @param specialistId identifier of the specialist
+     * @return institution associated with a specific specialist
+     */
+    Institution getSpecialistInstitution(String specialistId);
+
+    /**
+     * Get institution associated with a specific manager
+     * @param managerId identifier of the manager
+     * @return institution associated with a specific manager
+     */
+    Institution getManagerInstitution(String managerId);
+
+    /**
+     * @param institutionId identifier of the institution
+     * @return number of students an institution has
+     */
+    int countInstitutionStudents(String institutionId);
+
+    /**
+     * @param institutionId identifier of the institution
+     * @return number of students an institution has
+     */
+    int countInstitutionSpecialists(String institutionId);
 }
--- Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/services/InstitutionsService.java
+++ Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/services/InstitutionsService.java
@@ -1,5 +1,6 @@
 package pt.uminho.di.chalktyk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.hibernate.service.spi.ServiceException;
@@ -9,6 +10,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import pt.uminho.di.chalktyk.models.nonrelational.institutions.Institution;
+import pt.uminho.di.chalktyk.models.relational.Specialist;
+import pt.uminho.di.chalktyk.models.relational.Student;
 import pt.uminho.di.chalktyk.repositories.nonrelational.InstitutionDAO;
 import pt.uminho.di.chalktyk.repositories.relational.InstitutionSqlDAO;
 import pt.uminho.di.chalktyk.services.exceptions.BadInputException;
@@ -60,7 +63,7 @@ public class InstitutionsService implements IInstitutionsService {
                 Institution res = obj.get();
                 res.setDescription(body.getDescription());
                 res.setLogoPath(body.getLogoPath());
-                res.setSubscription(body.getSubscription());
+                res.setSubscription(body.getSubscription()); // TODO - a subscricao nao devia ser alterada aqui
                 idao.save(res);
             }
             else
@@ -73,18 +76,176 @@ public class InstitutionsService implements IInstitutionsService {
     @Override
     public void createInstitution(Institution institution) throws BadInputException {
         if (institution != null){
-            if (institution.getName() == null)
-                throw new BadInputException("Can't create institution: institution id is null");
+            if (institution.getName() == null || institution.getName().isEmpty())
+                throw new BadInputException("Can't create institution: institution's identifier is null/empty.");
             if (!idao.existsById(institution.getName())){
+                // TODO - verificar a parte da subscricao
                 idao.save(institution);
                 pt.uminho.di.chalktyk.models.relational.Institution sqlInst = new pt.uminho.di.chalktyk.models.relational.Institution(institution.getName());
                 isqldao.save(sqlInst);
             }
             else
-                throw new BadInputException("Can't create institution: id is alrealy in use");
+                throw new BadInputException("Can't create institution: id is already in use");
         }
         else 
             throw new BadInputException("Can't create institution: institution is null!");
+    }
+
+    /**
+     * Add specialists to institution
+     *
+     * @param institutionId  identifier of the institution
+     * @param specialistsIds list of specialists identifiers
+     */
+    @Override
+    public void addSpecialistsToInstitution(String institutionId, List<String> specialistsIds) {
+        // TODO
+    }
+
+    /**
+     * Add students to institution.
+     *
+     * @param institutionId identifier of the institution
+     * @param studentsIds   list of students identifiers
+     */
+    @Override
+    public void addStudentsToInstitution(String institutionId, List<String> studentsIds) {
+        // TODO
+    }
+
+    /**
+     * Remove specialists from institution
+     *
+     * @param institutionId  identifier of the institution
+     * @param specialistsIds list of specialists identifiers
+     */
+    @Override
+    public void removeSpecialistsFromInstitution(String institutionId, List<String> specialistsIds) {
+        // TODO
+    }
+
+    /**
+     * Remove students from institution.
+     *
+     * @param institutionId identifier of the institution
+     * @param studentsIds   list of students identifiers
+     */
+    @Override
+    public void removeStudentsFromInstitution(String institutionId, List<String> studentsIds) {
+        // TODO
+    }
+
+    /**
+     * Checks if a student is associated with a specific institution.
+     *
+     * @param studentId     identifier of the student
+     * @param institutionId identifier of the institution
+     * @return 'true' if the student is associated with the institution
+     */
+    @Override
+    public boolean isStudentOfInstitution(String studentId, String institutionId) {
+        return false; // TODO
+    }
+
+    /**
+     * Checks if a specialist is associated with a specific institution.
+     *
+     * @param specialistId  identifier of the specialist
+     * @param institutionId identifier of the institution
+     * @return 'true' if the specialist is associated with the institution
+     */
+    @Override
+    public boolean isSpecialistOfInstitution(String specialistId, String institutionId) {
+        return false; // TODO
+    }
+
+    /**
+     * Checks if a manager is associated with a specific institution.
+     *
+     * @param managerId     identifier of the manager
+     * @param institutionId identifier of the institution
+     * @return 'true' if the manager is associated with the institution
+     */
+    @Override
+    public boolean isManagerOfInstitution(String managerId, String institutionId) {
+        return false; // TODO
+    }
+
+    /**
+     * Get list of students that are associated with a specific institution.
+     *
+     * @param institutionId institution identifier
+     * @param page          index of the page
+     * @param itemsPerPage  number of items each page has
+     * @return list of students that are associated with a specific institution.
+     */
+    @Override
+    public Page<Student> getInstitutionStudents(String institutionId, int page, int itemsPerPage) {
+        return null; // TODO
+    }
+
+    /**
+     * Get list of specialists that are associated with a specific institution.
+     *
+     * @param institutionId institution identifier
+     * @param page          index of the page
+     * @param itemsPerPage  number of items each page has
+     * @return list of specialists that are associated with a specific institution.
+     */
+    @Override
+    public Page<Specialist> getInstitutionSpecialists(String institutionId, int page, int itemsPerPage) {
+        return null; // TODO
+    }
+
+    /**
+     * Get institution associated with a specific student
+     *
+     * @param studentId identifier of the student
+     * @return institution associated with a specific student
+     */
+    @Override
+    public Institution getStudentInstitution(String studentId) {
+        return null; // TODO
+    }
+
+    /**
+     * Get institution associated with a specific specialist
+     *
+     * @param specialistId identifier of the specialist
+     * @return institution associated with a specific specialist
+     */
+    @Override
+    public Institution getSpecialistInstitution(String specialistId) {
+        return null; // TODO
+    }
+
+    /**
+     * Get institution associated with a specific manager
+     *
+     * @param managerId identifier of the manager
+     * @return institution associated with a specific manager
+     */
+    @Override
+    public Institution getManagerInstitution(String managerId) {
+        return null; // TODO
+    }
+
+    /**
+     * @param institutionId identifier of the institution
+     * @return number of students an institution has
+     */
+    @Override
+    public int countInstitutionStudents(String institutionId) {
+        return 0; // TODO
+    }
+
+    /**
+     * @param institutionId identifier of the institution
+     * @return number of students an institution has
+     */
+    @Override
+    public int countInstitutionSpecialists(String institutionId) {
+        return 0; // TODO
     }
 
 }
