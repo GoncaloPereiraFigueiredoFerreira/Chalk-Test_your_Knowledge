f941ed2203277a4b6a8ce83fb480dc97f4ef2f81
Luis Silva <56602110+LuisMPSilva01@users.noreply.github.com>
2023-12-03 16:03:16 +0000

    Update InstitutionSqlDAO.java

--- Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/repositories/relational/InstitutionSqlDAO.java
+++ Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/repositories/relational/InstitutionSqlDAO.java
@@ -14,9 +14,26 @@ import pt.uminho.di.chalktyk.models.relational.Institution;
 public interface InstitutionSqlDAO extends JpaRepository<Institution, String> {
     @Modifying
     @Query(value = "UPDATE Specialist s SET s.institution.id = :institutionId WHERE s.id = :specialistId")
-    void addSpecialistToInstitution(@Param("specialistId") String specialistId, @Param("institutionId") String institution);
+    void addSpecialistToInstitution(@Param("specialistId") String specialistId, @Param("institutionId") String institutionId);
 
+    @Modifying
+    @Query(value = "UPDATE Student s SET s.institution.id = :institutionId WHERE s.id = :studentId")
+    void addStudentToInstitution(@Param("studentId") String studentId, @Param("institutionId") String institutionId);
+
+    @Modifying
+    @Query(value = "UPDATE Specialist s SET s.institution.id = null WHERE s.id = :specialistId and s.institution.id =:institutionId")
+    void removeSpecialistFromInstitution(@Param("specialistId") String specialistId, @Param("institutionId") String institutionId);
+
+    @Modifying
+    @Query(value = "UPDATE Student s SET s.institution.id = null WHERE s.id = :studentId and s.institution.id =:institutionId")
+    void removeStudentFromInstitution(@Param("studentId") String studentId, @Param("institutionId") String institutionId);
 
     @Query(value = "SELECT s.institution FROM Specialist s WHERE s.id = :specialistId")
     Institution getInstitutionBySpecialistId(@Param("specialistId") String specialistId);
+
+    @Query(value = "SELECT s.institution FROM Student s WHERE s.id = :studentId")
+    Institution getInstitutionByStudentId(@Param("studentId") String studentId);
+
+    @Query(value = "SELECT im.institution FROM InstitutionManager im WHERE im.id = :institutionManagerId")
+    Institution getInstitutionByInstitutionManagerId(@Param("institutionManagerId") String institutionManagerId);
 }
