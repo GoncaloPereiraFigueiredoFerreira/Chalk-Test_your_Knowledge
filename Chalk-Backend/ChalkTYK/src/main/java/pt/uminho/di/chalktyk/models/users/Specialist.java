d422b1bffb20689d51ae5b75f0458af0fe7d5cdf
RayMightBeWrong <ruizinho7braga@gmail.com>
2023-12-23 13:29:34 +0000

    Users TABLE_PER_CLASS

--- Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/models/users/Specialist.java
+++ Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/models/users/Specialist.java
@@ -13,7 +13,6 @@ import pt.uminho.di.chalktyk.models.institutions.Institution;
 @Setter
 @Entity
 @Table(name="Specialist")
-@DiscriminatorValue("Specialist")
 public class Specialist extends User {
     public Specialist(String id, String name, String photoPath, String email, String description) {
         super(id, name, photoPath, email, description);
