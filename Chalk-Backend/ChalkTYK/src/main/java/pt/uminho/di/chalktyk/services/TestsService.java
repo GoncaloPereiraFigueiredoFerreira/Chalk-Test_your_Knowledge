7c03e72678be63b14c4db396858da307746b2def
Luis Silva <lisinho2001@gmail.com>
2024-01-02 23:27:32 +0000

    Update TestsService.java

--- Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/services/TestsService.java
+++ Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/services/TestsService.java
@@ -568,6 +568,11 @@ public class TestsService implements ITestsService {
         testDAO.save(lt);
     }
 
+    @Override
+    public void automaticCorrection(String testId, String correctionType) throws NotFoundException, BadInputException, UnauthorizedException {
+
+    }
+
     // TODO - Para corrigir automaticamente um teste, pode ser necessário a combinacao de AI e a correcao automatica.
     //  Quando isto for invocado, corrigir apenas os que dão para ser corrigidos?
     @Transactional
