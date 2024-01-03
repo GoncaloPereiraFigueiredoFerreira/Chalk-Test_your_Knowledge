aeccbc9b966d74c2401846cd3e4c1c1d2b155cda
GoncaloPereiraFigueiredoFerreira <a93218@alunos.uminho.pt>
2024-01-03 20:40:20 +0000

    Merge with chico-frontend

--- Chalk-Frontend/src/components/objects/Rubric/Rubric.tsx
+++ Chalk-Frontend/src/components/objects/Rubric/Rubric.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { RubricEdit } from "./RubricEdit";
 import { RubricPreview } from "./RubricPreview";
 
@@ -59,39 +58,4 @@ export function Rubric({ context, rubric }: RubricProps) {
     case RubricContext.EDIT:
       return RubricEdit(rubric);
   }
-=======
-import {
-  Exercise,
-  ExerciseJustificationKind,
-  ExerciseType,
-  Resolution,
-} from "../Exercise/Exercise";
-import { useState, useEffect } from "react";
-import "./FBRubric";
-import "./OARubric";
-import "./MCRubric";
-
-export interface RubricProps {
-  type: ExerciseType;
-}
-
-export function Rubric({ type }: RubricProps) {
-  const [preview, setPreview] = useState(<></>);
-
-  useEffect(() => {
-    switch (type) {
-      case ExerciseType.MULTIPLE_CHOICE:
-        setPreview(<MCRubric></MCRubric>);
-        break;
-      case ExerciseType.OPEN_ANSWER:
-        setPreview(<OARubric></OARubric>);
-        break;
-      case ExerciseType.TRUE_OR_FALSE:
-        setPreview(<MCRubric></MCRubric>);
-        break;
-    }
-  }, [solution]);
-
-  return <div>{preview}</div>;
->>>>>>> origin/chico-frontend
 }
