3e94eefec9c5dfcb507aa7f5cfd0b6035c022398
Casal Novo <52653136+DCasalNovo@users.noreply.github.com>
2024-01-19 16:50:58 +0000

    Update WebApp.tsx

--- Chalk-Frontend/src/WebApp.tsx
+++ Chalk-Frontend/src/WebApp.tsx
@@ -10,11 +10,15 @@ export function WebApp() {
       <div
         className={`${
           isOpen ? "min-w-[256px]" : "min-w-[64px]"
-        } flex overflow-hidden transition-all duration-700`}
+        } flex overflow-hidden transition-all`}
       >
         <Sidebar isOpen={isOpen} toggle={toggle}></Sidebar>
       </div>
-      <div className="z-10 h-full w-full">
+      <div
+        className={`${
+          isOpen ? "w-[calc(100vw-256px)]" : "w-[calc(100vw-64px)]"
+        } z-10 h-full`}
+      >
         <Outlet />
       </div>
       <div className="fixed top-0 left-0 h-screen w-screen bg-gray-300 dark:bg-gray-800 -z-10"></div>
