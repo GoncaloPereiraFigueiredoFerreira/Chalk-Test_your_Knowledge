1cfbc902c7325f4e9fa9fb1d7da2cd0f0a5fb6cd
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2024-01-20 16:05:36 +0000

    error fixes

--- Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/dtos/TrueOrFalseAIDTO.java
+++ Chalk-Backend/ChalkTYK/src/main/java/pt/uminho/di/chalktyk/dtos/TrueOrFalseAIDTO.java
@@ -17,14 +17,18 @@ public class TrueOrFalseAIDTO {
     private TrueOrFalse[] list;
 
     public TrueOrFalseAIDTO(JsonArray jList) {
-        TrueOrFalse[] list = new TrueOrFalse[jList.size()];
+        list = new TrueOrFalse[jList.size()];
+
+        System.out.println("Lista = " + jList.toString());
 
         for(int i = 0; i < jList.size();i++){
             JsonObject obj = jList.getJsonObject(i); 
+            System.out.println(":" + obj.getString("Question") + " " + obj.getBoolean("True")); 
             list[i] = new TrueOrFalse(obj.getString("Question"), obj.getBoolean("True"));
         }
     }
 
+    @Data
     @AllArgsConstructor
     private class TrueOrFalse {
         
