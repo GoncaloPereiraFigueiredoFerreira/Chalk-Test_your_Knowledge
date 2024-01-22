3f2c76b584fbef976732d825c264af4050dae185
Casal Novo <52653136+DCasalNovo@users.noreply.github.com>
2024-01-22 21:37:14 +0000

    Update TestPage.tsx

--- Chalk-Frontend/src/components/pages/Tests/TestList/TestPage.tsx
+++ Chalk-Frontend/src/components/pages/Tests/TestList/TestPage.tsx
@@ -1,12 +1,13 @@
-import { Dropdown } from "flowbite-react";
 import { ListTests, ViewType } from "../../../objects/ListTests/ListTest.tsx";
 import { Searchbar } from "../../../objects/Searchbar/Searchbar.tsx";
 import { useContext, useState } from "react";
-import { GridIcon, ListIcon } from "../../../objects/SVGImages/SVGImages.tsx";
+import { FaListUl } from "react-icons/fa";
+import { HiViewGrid } from "react-icons/hi";
 import { UserContext, UserRole } from "../../../../UserContext.tsx";
 
 export function TestPage() {
   const [view, setViewType] = useState(ViewType.GRID);
+  const [kindOfTest, setKindOfTest] = useState<"Public" | "Private">("Public");
   const [searchKey, setSearch] = useState("");
   const { user } = useContext(UserContext);
 
@@ -14,54 +15,66 @@ export function TestPage() {
     <div className="flex flex-row divide-x-2 border-black dark:border-black divide-[#dddddd] dark:divide-[#dddddd]">
       <div className="flex flex-col w-full h-screen overflow-auto bg-white dark:bg-black min-h-max px-8 pb-8">
         <Searchbar setSearch={setSearch}></Searchbar>
-        <div className="flex w-full justify-between px-4 pb-6 mb-3 border-b-2 border-black dark:border-black divide-[#dddddd] dark:divide-[#dddddd]">
-          <label className="flex text-4xl text-gray-600 dark:text-white">
-            Testes
-          </label>
-          <div className="flex space-x-4">
-            <Dropdown
-              label=""
-              dismissOnClick={false}
-              renderTrigger={() => (
-                <button className="transition-all duration-100 py-2 px-4 rounded-lg bg-[#acacff] hover:bg-[#5555ce] dark:bg-gray-600 hover:dark:bg-[#ffd025] text-black hover:text-white dark:text-white hover:dark:text-black">
-                  {view === ViewType.GRID ? "Grelha" : "Lista"}
-                </button>
-              )}
-            >
-              <Dropdown.Item
-                as="a"
-                className="flex space-x-2"
-                onClick={() => setViewType(ViewType.LIST)}
-              >
-                <ListIcon />
-                <p>Lista</p>
-              </Dropdown.Item>
-              <Dropdown.Item
-                as="a"
-                className="flex space-x-2"
-                onClick={() => setViewType(ViewType.GRID)}
-              >
-                <GridIcon />
-                <p>Grelha</p>
-              </Dropdown.Item>
-            </Dropdown>
+        <div className="flex flex-col w-full gap-4 min-h-max">
+          <div className="flex w-full justify-between px-4 pb-6 mb-3 border-b-2 border-[#bbbbbb] dark:border-slate-600">
+            <label className="flex text-4xl text-slate-600 dark:text-white">
+              {kindOfTest === "Public" ? "Testes públicos" : "Os meus testes"}
+            </label>
             <button
-              className="transition-all duration-100 py-2 px-4 rounded-lg bg-[#acacff] hover:bg-[#5555ce] dark:bg-gray-600 hover:dark:bg-[#ffd025] text-black hover:text-white dark:text-white hover:dark:text-black"
+              className="py-2 px-4 text-base rounded-lg font-medium bg-[#acacff] hover:bg-[#5555ce] dark:bg-slate-600 hover:dark:bg-[#ffd025] text-black hover:text-white dark:text-white hover:dark:text-black transition-all duration-100"
               onClick={() => {}}
             >
               Criar Teste
             </button>
           </div>
+          <div className="flex justify-between">
+            <div className="flex gap-4">
+              {kindOfTest === "Private" ? (
+                <button
+                  onClick={() => setKindOfTest("Public")}
+                  className="flex w-fit items-center gap-2 py-2 px-4 text-base rounded-lg font-medium bg-[#acacff] hover:bg-[#5555ce] dark:bg-slate-600 hover:dark:bg-[#ffd025] text-black hover:text-white dark:text-white hover:dark:text-black transition-all duration-100"
+                >
+                  Testes públicos
+                </button>
+              ) : (
+                <button
+                  onClick={() => setKindOfTest("Private")}
+                  className="flex w-fit items-center gap-2 py-2 px-4 text-base rounded-lg font-medium bg-[#acacff] hover:bg-[#5555ce] dark:bg-slate-600 hover:dark:bg-[#ffd025] text-black hover:text-white dark:text-white hover:dark:text-black transition-all duration-100"
+                >
+                  Os meus testes
+                </button>
+              )}
+            </div>
+            <div className="flex gap-4">
+              {view === ViewType.GRID ? (
+                <button
+                  className="flex w-fit items-center gap-2 py-2 px-4 text-base rounded-lg font-medium bg-[#acacff] hover:bg-[#5555ce] dark:bg-slate-600 hover:dark:bg-[#ffd025] text-black hover:text-white dark:text-white hover:dark:text-black transition-all duration-100"
+                  onClick={() => setViewType(ViewType.LIST)}
+                >
+                  <FaListUl className="size-5 scale-90" />
+                  <p>Lista</p>
+                </button>
+              ) : (
+                <button
+                  className="flex w-fit items-center gap-2 py-2 px-4 text-base rounded-lg font-medium bg-[#acacff] hover:bg-[#5555ce] dark:bg-slate-600 hover:dark:bg-[#ffd025] text-black hover:text-white dark:text-white hover:dark:text-black transition-all duration-100"
+                  onClick={() => setViewType(ViewType.GRID)}
+                >
+                  <HiViewGrid className="size-5 scale-110" />
+                  <p>Grelha</p>
+                </button>
+              )}
+            </div>
+          </div>
+          <ListTests
+            view={view}
+            courseId={""}
+            visibilityType={
+              user.user?.role === UserRole.SPECIALIST ? "" : "public"
+            }
+            searchKey={searchKey}
+            tagsList={[]}
+          ></ListTests>
         </div>
-        <ListTests
-          view={view}
-          courseId={""}
-          visibilityType={
-            user.user?.role === UserRole.SPECIALIST ? "" : "public"
-          }
-          searchKey={searchKey}
-          tagsList={[]}
-        ></ListTests>
       </div>
     </div>
   );
