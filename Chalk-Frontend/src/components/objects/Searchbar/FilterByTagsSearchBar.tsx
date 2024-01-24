07d842f805bc1b44e9bd5d6645199ce426d2a15d
Casal Novo <52653136+DCasalNovo@users.noreply.github.com>
2024-01-24 20:45:41 +0000

    exercise cotation fix

--- Chalk-Frontend/src/components/objects/Searchbar/FilterByTagsSearchBar.tsx
+++ Chalk-Frontend/src/components/objects/Searchbar/FilterByTagsSearchBar.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Badge } from "flowbite-react";
 import { IoSearch } from "react-icons/io5";
 import { FaFilter } from "react-icons/fa";
 import "./Searchbar.css";
